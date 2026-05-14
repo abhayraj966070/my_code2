4ebdfddf60145952f19a4699b68f3d0a1115bf07
Abhayraj doot <abhayraj966070@gmail.com>
2026-05-14 09:18:22 +0530

    Add files via upload

---
+++ ARRAY3.java
@@ -0,0 +1,10 @@
+public class ARRAY3 {
+    public static void main(String[]args){
+        int arr[]={5, 10, 15, 20};
+        int sum=0;
+        for (int i = 0;i<arr.length; i++){
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+}
